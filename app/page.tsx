30d7d78efe33c5137b01fe407e7247fec97968e8
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-04-05 17:04:52 +0530

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Encryption from "@/components/main/Encryption";
 import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 
  
@@ -11,6 +12,7 @@ export default function Home() {
         <Hero/>
         <Skills/>
         <Encryption />
+        <Projects/>
 
       </div>
       
--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -3,7 +3,7 @@ import HeroContent from '../sub/HeroContent'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col h-full w-full'> 
+    <div className='flex flex-col h-full w-full' id='about-me'> 
     <video autoPlay muted loop className='rotate-18- absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'>
         <source src='/blackhole.webm' type='video/webm'/>
     </video>
