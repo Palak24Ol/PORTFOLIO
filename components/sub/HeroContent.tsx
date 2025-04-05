f6e53365ee3ce4e7badf811d15820fa9bea2bf44
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-04-05 14:42:20 +0530

    initial

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -23,7 +23,7 @@ const HeroContent = () => {
         </motion.div>
         <motion.div 
         variants={slideInFromLeft(0.5)}
-        className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto "
+        className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto "
         >
           Palak Jaiswal
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">Developer</span>
